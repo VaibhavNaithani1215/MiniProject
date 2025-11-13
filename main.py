8b007d6aacaa45f3cb18e9f556c9365ea3588e77
VaibhavNaithani1215 <naithanivaibhav68@gmail.com>
2025-11-13 14:31:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 
-password=input("Enter a strong password : ")
 
-a=any(char.isupper() for char in password)
-b=any(char.islower() for char in password)
-c=any(char.isdigit() for char in password)
-d=any(char in "!@#$%^&*()" for char in password)
+print("------MENU DRIVEN CALCULATOR------")
+
+while True:
+    print("Select operation")
+    print("1 for addition")
+    print("2 for subtraction")
+    print("3 for division")
+    print("4 for multiplication")
+    print("0 for exit")
+    choice=int(input("Enter your operation : "))
+
+    if choice==0:
+        print("EXIT")
+        break
+
+    elif choice==1:
+        a=int(input("Enter your number : "))
+        b=int(input("Enter your number : "))
+        print(f"Addition : {a+b}")
+        
+    elif choice==2:
+        a=int(input("Enter your number : "))
+        b=int(input("Enter your number : "))
+        print(f"Subtraction : {a-b}")
+        
+    elif choice==3:
+        a=int(input("Enter your number : "))
+        b=int(input("Enter your number : "))
+        if b==0:
+            print("Division by zero is not defined")
+        else:
+            print(f"Division : {a/b}")
+
+        
+        
+    elif choice==4:
+        a=int(input("Enter your number : "))
+        b=int(input("Enter your number : "))
+        print(f"Multiplication : {a*b}")
 
 
-score = 0
-if a: score += 1
-if b: score += 1
-if c: score += 1
-if d: score += 1
+    else:
+        print("invalid input")
+        
 
-if(len(password)<8):
-    print("Password is too small")
-elif(score==2):
-    print("🙂 Weak password,try somthing strong")
-elif(score==3):
-    print("🤏 Moderate password,try somthing strong")
-elif(score==4):
-    print("💪 Strong password")
-
-
-
-
+        
+        
